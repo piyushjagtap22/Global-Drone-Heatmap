45288c139918aadb72ae46ed0c9ef129deea269d
kirtish10 <kirtish10@github.com>
2021-05-11 19:04:20 +0530

    plot data till 10/5/21

--- app/app.js
+++ app/app.js
@@ -8,140 +8,190 @@ import { ScatterplotLayer } from '@deck.gl/layers';
 import { HeatmapLayer } from '@deck.gl/aggregation-layers';
 import { HexagonLayer } from '@deck.gl/aggregation-layers';
 import {AmbientLight, PointLight, LightingEffect} from '@deck.gl/core';
-
-
-
 import { Deck } from '@deck.gl/core';
-
-
 import { load } from '@loaders.gl/core';
 import { CSVLoader } from '@loaders.gl/csv';
-
+import {csv2geojson} from 'csv2geojson'
 // Set your mapbox token here
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2lydGlzaCIsImEiOiJja2N4cjNvY2YwMnAzMnltdjQ0dzd0NHN0In0.F5BaepKPJjvC_Rp8rHUVyA'; // eslint-disable-line
 
-// const lat = object.position[1];
-// const lng = object.position[0];
-// const count = object.points.length;
-
-const ambientLight = new AmbientLight({
-  color: [255, 255, 255],
-  intensity: 1.0
-});
-
-const pointLight1 = new PointLight({
-  color: [255, 255, 255],
-  intensity: 0.8,
-  radiusPixels: 2,
-  position: [-0.144528, 49.739968, 80000]
-});
-
-const pointLight2 = new PointLight({
-  color: [255, 255, 255],
-  intensity: 0.8,
-  position: [-3.807751, 54.104682, 8000]
-});
-
-const lightingEffect = new LightingEffect({ambientLight, pointLight1, pointLight2});
-
-const material = {
-  ambient: 0.64,
-  diffuse: 0.6,
-  shininess: 32,
-  specularColor: [51, 51, 51]
-};
-
-
-const colorScale = scaleLog()
-  .domain([10, 100, 1000, 10000])
-  .range([[255, 255, 178], [254, 204, 92], [253, 141, 60], [227, 26, 28]]);
-
- 
-
 export function renderToDOM(container, data) {
+
   const map = new mapboxgl.Map({
     container,
     style: 'mapbox://styles/mapbox/dark-v9',
     antialias: true,
-    center: [65.186647481789, 39.660324764558123],
+    center: [23.186647481789, 77.660324764558123],
     zoom: 1.8,
-   
     type: 'heatmap'
-  });78563698234931
-
+  });
+ console.log()
   map.addControl(new mapboxgl.NavigationControl(), 'top-left');
 
-  const radius = 1000;
-  const upperPercentile = 100
-  const coverage = 1
 
-  const deck = new Deck({
-    gl: map.painter.context.gl,
-    layers: [
+  // const deck = new Deck({
+
+  //   gl: map.painter.context.gl,
+  //   layers: [
      
-      new HeatmapLayer({
-        id: 'hex',
-        data: data,
-        getPosition: d => [d.lon, d.lat],
-        getWeight: d => 1,
-        radiusPixels: 60,        
-      
-      }),
+  //    new HeatmapLayer({
+  //       id: 'hex',
+  //       data: data,
+  //       getPosition: d => [parseInt(d[3]), parseInt(d[2])],
+  //       getWeight: d => 1,
+  //       radiusPixels: 15,
+        
+  //       aggregation: 'MEAN',
+  //       pickable: true,
+  //       threshold:0.16,
+  //       intensity:1,
+  //       colorRange:[[33,102,172,0], [103,169,207], [209,229,240], [253,219,199],[239,138,98],[178,4,43]],
+    
+  //     }),
+  //   ]
+  // });
 
+  // map.on('load', () => {
+  //  
+  // });
 
-      //   new HexagonLayer({
-      //     id: 'hex',
-      //     data: data,
-      //     getPosition: d => [d.lng, d.lat],
-      //     // getElevationWeight: d => 1,
-      //     elevationRange: [0, 3000],
-      //     elevationScale: data && data.length ? 50 : 0,
-      //     elevationScale: 100,
-      //     extruded: true,
-      //     radius: 1,         
-      //     opacity: 0.6,        
-      //     radius,
-      //     upperPercentile,
-      //     material,
-      //     coverage,
-      //     ambientLight,
-      // }),
-
-    //   new ScatterplotLayer({
-    //     id: 'my-scatterplot',
-    //     data: data,
-    //     getPosition: d => [d.lon,d.lat],
-    //     getRadius: d => 0.01,
-    //     getFillColor: [66, 135, 245],
-    //     opacity:0.5,
-    //     pickable: true,
-    // }),
-    ]
-  });
-
-  map.on('load', () => {
-    map.addLayer({
-      id: '3d-buildings',
-      source: 'composite',
-      'source-layer': 'building',
-      filter: ['==', 'extrude', 'true'],
-      type: 'fill-extrusion',
-      minzoom: 14,
-      paint: {
-        'fill-extrusion-color': '#ccc',
-        'fill-extrusion-height': ['get', 'height']
-      }
+  map.on('load',  () => {
+    // Add a geojson point source.
+    // Heatmap layers also work with a vector tile source.
+    console.log(data)
+    map.addSource('earthquakes', {
+        'type': 'geojson',
+        'data': '10_may_global_home_positions.geojson',
     });
-    // map.addLayer(new MapboxLayer({id: 'my-scatterplot', deck}));
-    map.addLayer(new MapboxLayer({ id: 'hex', deck }));
+    // map.addLayer(new MapboxLayer({ id: 'hex', deck }));
+    map.addLayer(
+        {
+            'id': 'earthquakes-heat',
+            'type': 'heatmap',
+            'source': 'earthquakes',
+            'maxzoom': 9,
+            'paint': {
+                // Increase the heatmap weight based on frequency and property magnitude
+                'heatmap-weight': {
+                  property: 'dbh',
+                  type: 'exponential',
+                  stops: [
+                    [1, 0],
+                    [62, 1]
+                  ]
+                },
+                // Increase the heatmap color weight weight by zoom level
+                // heatmap-intensity is a multiplier on top of heatmap-weight
+                'heatmap-intensity': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    0,
+                    1,
+                    9,
+                    3
+                ],
+                // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+                // Begin color ramp at 0-stop with a 0-transparancy color
+                // to create a blur-like effect.
+                'heatmap-color': [
+                    'interpolate',
+                    ['linear'],
+                    ['heatmap-density'],
+                    0,
+                    'rgba(33,102,172,0)',
+                    0.2,
+                    'rgb(103,169,207)',
+                    0.4,
+                    'rgb(209,229,240)',
+                    0.6,
+                    'rgb(253,219,199)',
+                    0.8,
+                    'rgb(239,138,98)',
+                    1,
+                    'rgb(178,24,43)'
+                ],
+                // Adjust the heatmap radius by zoom level
+                'heatmap-radius': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    0,
+                    2,
+                    9,
+                    20
+                ],
+                // Transition from heatmap to circle layer by zoom level
+                'heatmap-opacity': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    7,
+                    1,
+                    9,
+                    0
+                ]
+            }
+        },
+        'waterway-label'
+    );
 
+    map.addLayer(
+        {
+            'id': 'earthquakes-point',
+            'type': 'circle',
+            'source': 'earthquakes',
+            'minzoom': 7,
+            'paint': {
+                // Size circle radius by earthquake magnitude and zoom level
+                'circle-radius': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    7,
+                    ['interpolate', ['linear'], ['get', 'mag'], 1, 1, 6, 4],
+                    16,
+                    ['interpolate', ['linear'], ['get', 'mag'], 1, 5, 6, 50]
+                ],
+                // Color circle by earthquake magnitude
+                'circle-color': [
+                    'interpolate',
+                    ['linear'],
+                    ['get', 'mag'],
+                    1,
+                    'rgba(33,102,172,0)',
+                    2,
+                    'rgb(103,169,207)',
+                    3,
+                    'rgb(209,229,240)',
+                    4,
+                    'rgb(253,219,199)',
+                    5,
+                    'rgb(239,138,98)',
+                    6,
+                    'rgb(178,24,43)'
+                ],
+                'circle-stroke-color': 'white',
+                'circle-stroke-width': 1,
+                // Transition from heatmap to circle layer by zoom level
+                'circle-opacity': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    7,
+                    0,
+                    8,
+                    1
+                ]
+            }
+        },
+        'waterway-label'
+    );
+});
 
-
-    // renderLayers(map, data);
-  });
 
   return {
-    update: newData => renderLayers(map, newData),
+    update: newData => renderToDOM(map, newData),
     remove: () => {
       map.remove();
     }
@@ -151,12 +201,19 @@ export function renderToDOM(container, data) {
 
 
 export async function loadAndRender(container) {
-  // const data = await load(
-  //   'https://raw.githubusercontent.com/kirtish10/PX4_Global_Home_Positions/master/global_home_positions.csv',
-  //   CSVLoader
-  // );
-  const data = './global_home_positions.json'
-  // const data = await load("https://us-gun-violence.web.app/gundata.json");
-  console.log(typeof(data));
+  const data = await load(
+    'https://raw.githubusercontent.com/kirtish10/PX4_Global_Home_Positions/master/10_may_global_home_positions.csv',
+    CSVLoader
+  );
+
+await csv2geojson(data, function(err, data) {
+  console.log(err);
+  console.log(data);
   renderToDOM(container, data);
+  
+});
+
+  // const data = 'global_home_positions.json'
+  // const data = await load("https://us-gun-violence.web.app/gundata.json");
+ 
 }
