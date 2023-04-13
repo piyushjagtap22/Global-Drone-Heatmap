a9f837ef8f6833db3ae091ffef5664d0ac9d5554
piyushjagtap22 <jagspiyush@gmail.com>
2023-04-14 01:08:10 +0530

    updated variables

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const app = express();
-const routePrefix = process.env.ROUTE_PREFIX || '';
+const ejs = require('ejs')
+require('dotenv').config();
 
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
 
-app.get(routePrefix + '/heatmap', (req, res) => {
-  res.sendFile('index.html', { root: __dirname });
+app.get('/heatmap', (req, res) => {
+  const mapboxToken = process.env.MAPBOX_TOKEN;
+  res.render(__dirname + "/index.html", {token:mapboxToken});
 });
 
 app.get('/data.geojson', (req, res) => {
   res.sendFile('data.geojson', { root: __dirname });
 });
 
-const server = app.listen(0, () => {
-  const port = server.address().port;
-  console.log(port);
+const server = app.listen(3000, () => {
+  //const port = server.address().port;
+  //console.log(port);
+  console.log('running on 3000')
 });
