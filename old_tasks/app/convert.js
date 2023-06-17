ea0e64b1e95575ba1a42172a35853420ccda912c
piyushjagtap22 <jagspiyush@gmail.com>
2023-06-17 19:37:07 +0530

    test deploy 1

--- old_tasks/app/convert.js
+++
@@ -1,9 +0,0 @@
-
-const csvToJson = require('convert-csv-to-json');
- 
-const input = 'global_home_positions.csv'; 
-const output = './global_home_positions.json';
- 
-csvToJson.fieldDelimiter(',')
-         .formatValueByType()
-         .generateJsonFileFromCsv(input, output);
